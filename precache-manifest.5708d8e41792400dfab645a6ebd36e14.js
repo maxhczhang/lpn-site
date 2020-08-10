ca9da46b9a278b740c22c7dcb7c45bf70df399d3
Ryan Miranda <ryanm7082@gmail.com>
2020-08-09 20:44:29 -0700

    Updates

---
+++ precache-manifest.5708d8e41792400dfab645a6ebd36e14.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46e6a9e7e270f00c4b02a1adb314a80",
+    "url": "/lpn-site/index.html"
+  },
+  {
+    "revision": "8fabfab86df2ba48a0ba",
+    "url": "/lpn-site/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "54e849f3014069a00a83",
+    "url": "/lpn-site/static/css/main.0fb8338c.chunk.css"
+  },
+  {
+    "revision": "8fabfab86df2ba48a0ba",
+    "url": "/lpn-site/static/js/2.6ae2490b.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/lpn-site/static/js/2.6ae2490b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e849f3014069a00a83",
+    "url": "/lpn-site/static/js/main.f8aecb37.chunk.js"
+  },
+  {
+    "revision": "d6967bd269e3b73fb0e6",
+    "url": "/lpn-site/static/js/runtime-main.00264591.js"
+  },
+  {
+    "revision": "59c8597f72041114e4be65c530b7229f",
+    "url": "/lpn-site/static/media/About.59c8597f.jpg"
+  },
+  {
+    "revision": "c87c8eba724191edd37a9efbca3cc32a",
+    "url": "/lpn-site/static/media/Accenture.c87c8eba.png"
+  },
+  {
+    "revision": "691e02fb147ae57b996bf015c3d6e5d8",
+    "url": "/lpn-site/static/media/Alex_Cole.691e02fb.jpg"
+  },
+  {
+    "revision": "80f40d6a0fdd62a13ff852ac578bec27",
+    "url": "/lpn-site/static/media/Amazon.80f40d6a.png"
+  },
+  {
+    "revision": "e10cace6fe0c8e7232a5101c8530e939",
+    "url": "/lpn-site/static/media/Andrew_Duong.e10cace6.jpg"
+  },
+  {
+    "revision": "0e7b8524f0307f6cbb3b49e338b9e64a",
+    "url": "/lpn-site/static/media/Astyr_Ko.0e7b8524.jpg"
+  },
+  {
+    "revision": "3154f8228359520d77811203e8c6e404",
+    "url": "/lpn-site/static/media/CampusInvolvements.3154f822.jpg"
+  },
+  {
+    "revision": "6a5faa3166322f17d79ddf4424f97b10",
+    "url": "/lpn-site/static/media/Careers.6a5faa31.jpeg"
+  },
+  {
+    "revision": "b7ba684e0236d79a079f3e12771b11cd",
+    "url": "/lpn-site/static/media/Careers_Small.b7ba684e.jpg"
+  },
+  {
+    "revision": "ac38ea0af405e041a1270c67a3886c0c",
+    "url": "/lpn-site/static/media/Catherine_Lin.ac38ea0a.jpg"
+  },
+  {
+    "revision": "65336cf2349ff6ad6c7f0b70d9cd3ee5",
+    "url": "/lpn-site/static/media/Catherine_Nguyen.65336cf2.jpg"
+  },
+  {
+    "revision": "be42feeaabb6d4598160c2baf09a48e2",
+    "url": "/lpn-site/static/media/Core_Values_Separator.be42feea.jpg"
+  },
+  {
+    "revision": "93e9f18675c8853e9d132be749b615e2",
+    "url": "/lpn-site/static/media/David_Sonq.93e9f186.jpg"
+  },
+  {
+    "revision": "e48af6d097316394b7868aadbc5da2eb",
+    "url": "/lpn-site/static/media/Deloitte.e48af6d0.png"
+  },
+  {
+    "revision": "651b4b14813aa8c99d895e07b49f2011",
+    "url": "/lpn-site/static/media/EY.651b4b14.png"
+  },
+  {
+    "revision": "5e83580a5072c803bafc67fdedec191d",
+    "url": "/lpn-site/static/media/Emily_Quynh_Nguyen.5e83580a.jpeg"
+  },
+  {
+    "revision": "4f60c0aca1154d924e0c2be9683f3ec0",
+    "url": "/lpn-site/static/media/Experian.4f60c0ac.png"
+  },
+  {
+    "revision": "d1a7d83755c3de32014f3359a34b9b61",
+    "url": "/lpn-site/static/media/Faith_Chia.d1a7d837.jpg"
+  },
+  {
+    "revision": "61cd7dd6d3d1c9527edcf02d29f392ab",
+    "url": "/lpn-site/static/media/Gallery.61cd7dd6.jpg"
+  },
+  {
+    "revision": "5b76ae214820a15e80208198ffd1e90a",
+    "url": "/lpn-site/static/media/Home.5b76ae21.jpg"
+  },
+  {
+    "revision": "9fc82c43b69e698cdcda064015874d81",
+    "url": "/lpn-site/static/media/Ian_Han.9fc82c43.jpg"
+  },
+  {
+    "revision": "ab1492566372f93942537ff82869db45",
+    "url": "/lpn-site/static/media/Iolites_small.ab149256.jpg"
+  },
+  {
+    "revision": "47853e8f877f7a1a9ed253637040f214",
+    "url": "/lpn-site/static/media/Iotas_Formals.47853e8f.jpg"
+  },
+  {
+    "revision": "74f54740a7959a8f34fcb0445c5b9cfe",
+    "url": "/lpn-site/static/media/Jasmin_He.74f54740.jpg"
+  },
+  {
+    "revision": "bd4dd5e09aa305525a40ebc4e705bdff",
+    "url": "/lpn-site/static/media/Jeffrey_Cho.bd4dd5e0.jpg"
+  },
+  {
+    "revision": "23446456d6b3dc0952675dc65118a3f0",
+    "url": "/lpn-site/static/media/Jenny_Cheung.23446456.jpg"
+  },
+  {
+    "revision": "c3b51aa6d0c0aac77828d071400ab625",
+    "url": "/lpn-site/static/media/Joshua_Tong.c3b51aa6.jpg"
+  },
+  {
+    "revision": "a2d856580e7482512ccc5cc3cbcb2828",
+    "url": "/lpn-site/static/media/Julian_Rachman.a2d85658.jpg"
+  },
+  {
+    "revision": "0d825c42daf2ade100b754671d7ef627",
+    "url": "/lpn-site/static/media/KPMG.0d825c42.png"
+  },
+  {
+    "revision": "21c40fcde5c574c49d281be231ec9ab8",
+    "url": "/lpn-site/static/media/Kappas_small.21c40fcd.jpg"
+  },
+  {
+    "revision": "f16a9b7122312020be137067e2a97531",
+    "url": "/lpn-site/static/media/Karen_Ching.f16a9b71.jpg"
+  },
+  {
+    "revision": "f26b97ad878bf1ce81bb1f3c780c8505",
+    "url": "/lpn-site/static/media/Kevins_Bday_2020.f26b97ad.JPG"
+  },
+  {
+    "revision": "d8d04671dc01a0eac844e971aa33e7a9",
+    "url": "/lpn-site/static/media/LPN_Letters_small.d8d04671.jpg"
+  },
+  {
+    "revision": "a2748dfe3473a89a1642af7ec47d3bd6",
+    "url": "/lpn-site/static/media/Le_An_Pham.a2748dfe.jpg"
+  },
+  {
+    "revision": "73f83bf22ab5d8f53945e36752457a05",
+    "url": "/lpn-site/static/media/Linda_Woo.73f83bf2.jpg"
+  },
+  {
+    "revision": "49ba21887b928a50126ef564f1313ead",
+    "url": "/lpn-site/static/media/Lucian_Liu.49ba2188.jpg"
+  },
+  {
+    "revision": "a68366dc3e3c1fef3fc2d2fc9bcaf180",
+    "url": "/lpn-site/static/media/Matthew_Nagata.a68366dc.jpg"
+  },
+  {
+    "revision": "ca8ed8dc5e79c48d4fde1c30a1b2c3fa",
+    "url": "/lpn-site/static/media/Microsoft.ca8ed8dc.png"
+  },
+  {
+    "revision": "641fd82ef8a6151352ed2c41689295c4",
+    "url": "/lpn-site/static/media/Naishal_Shah.641fd82e.jpg"
+  },
+  {
+    "revision": "ababd3285ca2338ae68a896e96f843d1",
+    "url": "/lpn-site/static/media/Omar_Naji.ababd328.jpg"
+  },
+  {
+    "revision": "4ea8155dd47264d6d2140bc0248ea173",
+    "url": "/lpn-site/static/media/Philip_Cheung.4ea8155d.jpg"
+  },
+  {
+    "revision": "cf484fd297e84f347446c1a511fd29a7",
+    "url": "/lpn-site/static/media/Raymond_Dinh.cf484fd2.jpg"
+  },
+  {
+    "revision": "8020b7823370bad3f5c0d56d787e8026",
+    "url": "/lpn-site/static/media/Ryan_Miranda.8020b782.jpg"
+  },
+  {
+    "revision": "deb7b49cd9af84f68a9789dd351dd428",
+    "url": "/lpn-site/static/media/Sahana_Vairavaraj.deb7b49c.jpg"
+  },
+  {
+    "revision": "ec84116fa581a29ae6416fbeba796e3c",
+    "url": "/lpn-site/static/media/Sandy_Nguyen.ec84116f.jpg"
+  },
+  {
+    "revision": "eb392b518797f8c5b1d649b52c0a39f4",
+    "url": "/lpn-site/static/media/Sera_Xu.eb392b51.jpg"
+  },
+  {
+    "revision": "528a4453d36ad6ecb4b5e52f233f404c",
+    "url": "/lpn-site/static/media/Serina_Nguyen.528a4453.jpg"
+  },
+  {
+    "revision": "4f5da93e35ce7507f2749c0929c62c85",
+    "url": "/lpn-site/static/media/Stephen_Minn.4f5da93e.jpg"
+  },
+  {
+    "revision": "825bb744d6c61938f25338b92e08dcae",
+    "url": "/lpn-site/static/media/Taiwan_1.825bb744.jpg"
+  },
+  {
+    "revision": "7f114c21fdd8de89be49e457bf08a570",
+    "url": "/lpn-site/static/media/Tiffany_Chan.7f114c21.jpg"
+  },
+  {
+    "revision": "8d9438984736411f5c3f05c4fa8184ba",
+    "url": "/lpn-site/static/media/Tiffany_Wong.8d943898.jpg"
+  },
+  {
+    "revision": "c51fd260e7ce91017e0994f1f013ae8c",
+    "url": "/lpn-site/static/media/Tony_Zhou.c51fd260.jpg"
+  },
+  {
+    "revision": "add7c7050bf7c32c96b29de70e865b72",
+    "url": "/lpn-site/static/media/Tyler_Newman.add7c705.jpg"
+  },
+  {
+    "revision": "1e4f29ca95a223c5dce08f475001c0e3",
+    "url": "/lpn-site/static/media/Vivi_Liu.1e4f29ca.jpg"
+  },
+  {
+    "revision": "9433889c5a24aed2d4d2b215d19e7230",
+    "url": "/lpn-site/static/media/Xi_Casual.9433889c.jpg"
+  },
+  {
+    "revision": "539c6b6b62f1aabe3c0d959a4fa969f4",
+    "url": "/lpn-site/static/media/Xi_Frat_Social.539c6b6b.JPG"
+  },
+  {
+    "revision": "4afc1086efb9540ac654951b6cfd0cd4",
+    "url": "/lpn-site/static/media/Yong_Lin.4afc1086.jpg"
+  },
+  {
+    "revision": "f68038e36a790eac4d9dc325b089325e",
+    "url": "/lpn-site/static/media/logo.f68038e3.png"
+  },
+  {
+    "revision": "391d31fd0b9d1a2c61f7049fbd60b0ab",
+    "url": "/lpn-site/static/media/logo_white.391d31fd.png"
+  },
+  {
+    "revision": "ba8f41ac9ff696452d846250c3166f49",
+    "url": "/lpn-site/static/media/merage_at_night.ba8f41ac.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lpn-site/precache-manifest.4d2792e1fb2b09df02f5ea8673bb453c.js"
+  "/lpn-site/precache-manifest.5708d8e41792400dfab645a6ebd36e14.js"
 );
 
 self.addEventListener('message', (event) => {
