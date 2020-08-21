171e3d4e0b4df1d1ee853344faf0d0cd296a7392
Ryan Miranda <ryanm7082@gmail.com>
2020-08-20 23:44:37 -0700

    snackbar on form submit

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -5,6 +5,10 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 
 import colorLogo from '../../../assets/lpn_assets/logo.png'
 
@@ -37,40 +41,61 @@ const useStyles = makeStyles((theme) => ({
     buttonWrapper: {
         display: 'flex',
         justifyContent: 'flex-end'
+    },
+    snack: {
+        backgroundColor: "#580C1F",
+        color: "white"
     }
 }));
 
 const isEmail = (_email) => {
-    return _email.includes("@") && _email.includes(".com")
+    return _email === '' || (_email.includes("@") && _email.includes(".com"))
 }
+
+const postToServer = async (newRow) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ newRow })
+    };
+
+    fetch('https://evening-savannah-62428.herokuapp.com/v1/sheets', requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data));
+};
+
 
 export default function Contact({ setPage }) {
     useEffect(() => {
         setPage("Contact")
     });
 
-    const postToServer = async (newRow) => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ newRow })
-        };
-
-        fetch('/v1/sheets', requestOptions)
-            .then(response => response.json())
-            .then(data => console.log(data));
-    };
-
     const classes = useStyles();
     const [name, setName] = useState('');
     const [year, setYear] = useState('');
-    const [email, setEmail] = useState('@gmail.com');
+    const [email, setEmail] = useState('');
     const [question, setQuestion] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const resetState = () => {
+        setName('');
+        setYear('');
+        setEmail('');
+        setQuestion('');
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const newRow = {Name: name, Year: year, Email: email, Question: question};
-        postToServer(newRow);
+        //postToServer(newRow).then(resetState);
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
     };
 
     return (
@@ -86,6 +111,7 @@ export default function Contact({ setPage }) {
                             name="firstName"
                             label="Name"
                             onInput={e => setName(e.target.value)}
+                            value={name}
                         />
                     </Grid>
                 
@@ -95,6 +121,7 @@ export default function Contact({ setPage }) {
                             name="year"
                             label="Year"
                             onInput={e => setYear(e.target.value)}
+                            value={year}
                         />
                     </Grid>
 
@@ -107,6 +134,7 @@ export default function Contact({ setPage }) {
                             onInput={e => setEmail(e.target.value)}
                             error={!isEmail(email)}
                             helperText={!isEmail(email) && "Enter a valid email."}
+                            value={email}
                         />
                     </Grid>
 
@@ -119,6 +147,7 @@ export default function Contact({ setPage }) {
                             style={{width:"70%"}}
                             multiline
                             onInput={e => setQuestion(e.target.value)}
+                            value={question}
                         />
                     </Grid>
 
@@ -128,6 +157,26 @@ export default function Contact({ setPage }) {
                     <Button className={classes.button} type="submit">Submit</Button>
                 </Box>
             </form>
+
+            <Snackbar
+                className={classes.snack}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+            >
+                <SnackbarContent 
+                    className={classes.snack} 
+                    message={"We'll get back to you soon!"} action={
+                        <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    }
+                ></SnackbarContent>
+            </Snackbar>
         </Box>
     )
 }
