d54397fc4fc461c3670d3801e061c030404ab85b
Ryan Miranda <ryanm7082@gmail.com>
2020-08-20 23:45:21 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lpn-site/precache-manifest.1046722d2e13eddf20d568b7e8a450c6.js"
+  "/lpn-site/precache-manifest.fe4874779076cf96e6687e54acc97ea8.js"
 );
 
 self.addEventListener('message', (event) => {
