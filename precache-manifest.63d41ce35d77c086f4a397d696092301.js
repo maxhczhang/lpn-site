d80fa76150f0518a398919b42af32ec93d34e188
Rithu Eswaramoorthy <rithue2000@gmail.com>
2021-09-23 16:51:00 -0700

    Updates

--- precache-manifest.63d41ce35d77c086f4a397d696092301.js
+++ precache-manifest.63d41ce35d77c086f4a397d696092301.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00089e1fc920b29acc203700005445b1",
+    "revision": "6150adffcde838464422151dc7824ed4",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.9905d55a.chunk.css"
   },
   {
-    "revision": "47fbb600e397f5f36cbf",
+    "revision": "10585b75567b368cef9a",
     "url": "/static/css/main.97268bb8.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.21b04418.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "47fbb600e397f5f36cbf",
-    "url": "/static/js/main.3efd026e.chunk.js"
+    "revision": "10585b75567b368cef9a",
+    "url": "/static/js/main.9eefb513.chunk.js"
   },
   {
     "revision": "9e76e5b190c1ab701af5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46d9567604efd0e298c2b1aecaca9844.js"
+  "/precache-manifest.63d41ce35d77c086f4a397d696092301.js"
 );
 
 self.addEventListener('message', (event) => {
