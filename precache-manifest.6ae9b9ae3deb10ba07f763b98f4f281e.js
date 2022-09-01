0c163b668a9c4e50aa28f567932a98eedccaf204
Jaejin Kim <jaejinkim@Jaejins-MacBook-Pro.local>
2022-09-02 03:06:29 +0900

    Updates

---
+++ precache-manifest.6ae9b9ae3deb10ba07f763b98f4f281e.js
@@ -0,0 +1,638 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d97a502de0ffa34f84efe91a98682ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a16c46a6af6f7fa4d2a",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "c4a8da19229f00b9b14c",
+    "url": "/static/css/main.97268bb8.chunk.css"
+  },
+  {
+    "revision": "2a16c46a6af6f7fa4d2a",
+    "url": "/static/js/2.c0c56832.chunk.js"
+  },
+  {
+    "revision": "51487bead9617837ea8a36c4e549c8d4",
+    "url": "/static/js/2.c0c56832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a8da19229f00b9b14c",
+    "url": "/static/js/main.33cdc574.chunk.js"
+  },
+  {
+    "revision": "9e76e5b190c1ab701af5",
+    "url": "/static/js/runtime-main.b86ae5a1.js"
+  },
+  {
+    "revision": "8339328f3c3e9125145ce9096dc8f30b",
+    "url": "/static/media/AA.8339328f.png"
+  },
+  {
+    "revision": "ec942d50d705a5350a1b660fc6e875a1",
+    "url": "/static/media/Aaron_Lee.ec942d50.jpg"
+  },
+  {
+    "revision": "80f6e0469f6ec9d0e1c29da945d73ae0",
+    "url": "/static/media/About.80f6e046.jpg"
+  },
+  {
+    "revision": "c457ca34fec957865c791c1d54d1695c",
+    "url": "/static/media/Accenture.c457ca34.png"
+  },
+  {
+    "revision": "cfa277adc3eb353f74d2d1ed3c33ff65",
+    "url": "/static/media/Accenture.cfa277ad.png"
+  },
+  {
+    "revision": "a2de43113145c040419fd9cc68301129",
+    "url": "/static/media/Adrian_Siu.a2de4311.jpg"
+  },
+  {
+    "revision": "1a7d775d8d218f2393fd8848a386ef6e",
+    "url": "/static/media/Amazon.1a7d775d.png"
+  },
+  {
+    "revision": "52fc8e31692630d38cef0f7e0b20bdbf",
+    "url": "/static/media/Amazon.52fc8e31.png"
+  },
+  {
+    "revision": "e5fc2f87c0f71730f8bff54022297139",
+    "url": "/static/media/Ashley_London.e5fc2f87.jpg"
+  },
+  {
+    "revision": "8a100ec488a19d14d497bf186d8f4b49",
+    "url": "/static/media/BAP.8a100ec4.png"
+  },
+  {
+    "revision": "3e1feee1d94abc284d0a09606868ca21",
+    "url": "/static/media/CapitalOne.3e1feee1.png"
+  },
+  {
+    "revision": "45e32db8c21b36fe1e6cf95ef8f0e668",
+    "url": "/static/media/Careers.45e32db8.jpeg"
+  },
+  {
+    "revision": "ac38ea0af405e041a1270c67a3886c0c",
+    "url": "/static/media/Catherine_Lin.ac38ea0a.jpg"
+  },
+  {
+    "revision": "76ce023bb6b3e259a1fa06cdd4b8a8e9",
+    "url": "/static/media/Catherine_Nguyen.76ce023b.jpg"
+  },
+  {
+    "revision": "936d23f3df8a67d30cffafb674c8d30f",
+    "url": "/static/media/Cathy_Yun.936d23f3.jpg"
+  },
+  {
+    "revision": "05acb219f0d0440a29e83d0dba1284ff",
+    "url": "/static/media/Comcast.05acb219.png"
+  },
+  {
+    "revision": "f5cd2078d46ce205e3e054ea9be27039",
+    "url": "/static/media/ComfortKeepers.f5cd2078.png"
+  },
+  {
+    "revision": "65d26d7330b4f874e447cd53d1839823",
+    "url": "/static/media/CoreValues_Separator.65d26d73.jpg"
+  },
+  {
+    "revision": "8b7ac8062437d165caf531a2f4f3e825",
+    "url": "/static/media/Courtney_Tsao.8b7ac806.jpg"
+  },
+  {
+    "revision": "a409bb617093ae68a1e09aa8cbdf0f13",
+    "url": "/static/media/Csuite_2021.a409bb61.jpeg"
+  },
+  {
+    "revision": "223b341bf4a6f59022c7b5bbe596d254",
+    "url": "/static/media/DayandNight.223b341b.webp"
+  },
+  {
+    "revision": "9fc766e0600196ea7e00f73dec16b9d0",
+    "url": "/static/media/Deena_Toofan.9fc766e0.jpg"
+  },
+  {
+    "revision": "0e7ee6d97375e305f5918e2415dc9397",
+    "url": "/static/media/Deloitte.0e7ee6d9.png"
+  },
+  {
+    "revision": "e48af6d097316394b7868aadbc5da2eb",
+    "url": "/static/media/Deloitte.e48af6d0.png"
+  },
+  {
+    "revision": "192b638419ea9dc5f06828cc491f4fa6",
+    "url": "/static/media/Disney.192b6384.png"
+  },
+  {
+    "revision": "9ac55274411238007333c8a68796be32",
+    "url": "/static/media/Dodge_for_dollars.9ac55274.jpg"
+  },
+  {
+    "revision": "5c2606fbce093ef79e06788959bedc13",
+    "url": "/static/media/EY.5c2606fb.png"
+  },
+  {
+    "revision": "f238e35443bb5adca21640a101029382",
+    "url": "/static/media/Ebay.f238e354.png"
+  },
+  {
+    "revision": "f8d55497d397b549fb6719b853dd4c7f",
+    "url": "/static/media/Eddie_Rios.f8d55497.jpg"
+  },
+  {
+    "revision": "209b5b15116d5f186bbee3f2b7c63e60",
+    "url": "/static/media/Elizabeth_Chan.209b5b15.jpg"
+  },
+  {
+    "revision": "6e9657b471ab96d793d316098ced0fd2",
+    "url": "/static/media/Etas_Seniors.6e9657b4.jpg"
+  },
+  {
+    "revision": "347b8434e4b821d864f60bfee8edc5de",
+    "url": "/static/media/Ethan_Canavan.347b8434.jpg"
+  },
+  {
+    "revision": "81c67940f69a1f26f9016379668aecf3",
+    "url": "/static/media/Excellence.81c67940.jpg"
+  },
+  {
+    "revision": "c18061c328ef7e27ac92edcb045f3972",
+    "url": "/static/media/Excellence.c18061c3.JPG"
+  },
+  {
+    "revision": "4f60c0aca1154d924e0c2be9683f3ec0",
+    "url": "/static/media/Experian.4f60c0ac.png"
+  },
+  {
+    "revision": "2af4b299552ad1aebeaa474c117138d5",
+    "url": "/static/media/Ey.2af4b299.png"
+  },
+  {
+    "revision": "b5856fb21624942690948022268ca38e",
+    "url": "/static/media/Faith_Chia.b5856fb2.jpg"
+  },
+  {
+    "revision": "0aa1b59abb2d2f5fe7ba0d86f9149461",
+    "url": "/static/media/Fall_Rush_Cover.0aa1b59a.png"
+  },
+  {
+    "revision": "a8fdc090b7d3caa7954b6b5fd6f61bc1",
+    "url": "/static/media/Fall_Talent_2020.a8fdc090.JPG"
+  },
+  {
+    "revision": "a68a20f7a9bbe5567ffa8995f0fd7874",
+    "url": "/static/media/Founders_Alt_1.a68a20f7.png"
+  },
+  {
+    "revision": "39c49364b8472251b63500408a7d9293",
+    "url": "/static/media/Gallery.39c49364.jpeg"
+  },
+  {
+    "revision": "446cc166b32603f07fc8ca7550c24abc",
+    "url": "/static/media/Gallery.446cc166.jpg"
+  },
+  {
+    "revision": "d6566127a7d29b34a960cc349daf3217",
+    "url": "/static/media/Goldman_Sachs.d6566127.png"
+  },
+  {
+    "revision": "f199f144f7cef72c8edb3a2951210e83",
+    "url": "/static/media/Google.f199f144.png"
+  },
+  {
+    "revision": "35445ffb9f9a616503d97ef1e91d95b0",
+    "url": "/static/media/Graduating_Seniors.35445ffb.jpg"
+  },
+  {
+    "revision": "5954ec3089911497102a0eb141a14aae",
+    "url": "/static/media/Group_Seniors.5954ec30.jpg"
+  },
+  {
+    "revision": "1a622e1747413d703add31b33298bee5",
+    "url": "/static/media/HRMA.1a622e17.png"
+  },
+  {
+    "revision": "1b86a2002377ca46c8f63308eabc863e",
+    "url": "/static/media/Haley_Kim.1b86a200.jpg"
+  },
+  {
+    "revision": "73ee572fd508434de903801c36d8e040",
+    "url": "/static/media/Hans_Chang.73ee572f.jpg"
+  },
+  {
+    "revision": "401ee7c23b0580687c9aae1fe3e6d576",
+    "url": "/static/media/Hanui_Lee.401ee7c2.jpg"
+  },
+  {
+    "revision": "5644508271be5d17b2a8d18b55f6b995",
+    "url": "/static/media/Hanui_Lee.56445082.jpg"
+  },
+  {
+    "revision": "d102c5eb4b1da2ebc8e4991bf7ebd0b5",
+    "url": "/static/media/Home.d102c5eb.jpg"
+  },
+  {
+    "revision": "d3bb9cd9093a035b86f7fb9f8d32cdf1",
+    "url": "/static/media/ISBA.d3bb9cd9.png"
+  },
+  {
+    "revision": "748148321048b50a2cd1714e67499d71",
+    "url": "/static/media/Ian_Han.74814832.jpg"
+  },
+  {
+    "revision": "96465a67e2bf4a438d48e1986fb28879",
+    "url": "/static/media/Intel.96465a67.png"
+  },
+  {
+    "revision": "e944bc7d9d2219db00606e14afc12344",
+    "url": "/static/media/Iolites_small.e944bc7d.jpg"
+  },
+  {
+    "revision": "0b53af50a716e8a76e4b59ed2a9ede4f",
+    "url": "/static/media/Iota_Friends_Giving.0b53af50.jpg"
+  },
+  {
+    "revision": "306445ac5d5beeda066820f7d453fe11",
+    "url": "/static/media/Iotas_Formals.306445ac.jpg"
+  },
+  {
+    "revision": "55cf1e697c0eddb9f6f525ce4a9ceade",
+    "url": "/static/media/Iotas_Seniors.55cf1e69.jpg"
+  },
+  {
+    "revision": "bd2868311bdd0647e43739c6fa24d83b",
+    "url": "/static/media/Jaejin_Kim.bd286831.jpg"
+  },
+  {
+    "revision": "f58456ad08172e28fc0ca0cb7b2ac00d",
+    "url": "/static/media/Jamie_Zhao.f58456ad.jpg"
+  },
+  {
+    "revision": "afa064af05cf81c43300eaa671c979f0",
+    "url": "/static/media/Jennifer_Lin.afa064af.jpg"
+  },
+  {
+    "revision": "9000605d35bc1f7b68da324340217365",
+    "url": "/static/media/Jenny_Cheung.9000605d.png"
+  },
+  {
+    "revision": "d4d490578231aae5fe9e57948839b357",
+    "url": "/static/media/Jenny_Jeffrey.d4d49057.jpg"
+  },
+  {
+    "revision": "9a49428765a3e662322e55454d11913c",
+    "url": "/static/media/Jodie_Tai.9a494287.jpg"
+  },
+  {
+    "revision": "f868279869a5b67320d8145c5756c1a1",
+    "url": "/static/media/Josh_Tong.f8682798.jpg"
+  },
+  {
+    "revision": "61475163cf8eaf848f906995468140dc",
+    "url": "/static/media/JpMorgan.61475163.png"
+  },
+  {
+    "revision": "6328b2f9fe6a66fb4a79aefb76224117",
+    "url": "/static/media/JpMorgan.6328b2f9.jpg"
+  },
+  {
+    "revision": "1c115ae0e06eac47bb397c66f35e6ce8",
+    "url": "/static/media/Julian_Rachman.1c115ae0.jpg"
+  },
+  {
+    "revision": "bdbf0419ba4dee0e7e0ba610d85f186e",
+    "url": "/static/media/Julie_Hsieh.bdbf0419.jpg"
+  },
+  {
+    "revision": "8d4ec9d4c0a18ebff4b879d60d49b5fb",
+    "url": "/static/media/Julio_Cano.8d4ec9d4.jpg"
+  },
+  {
+    "revision": "a1913366c92dc66c1e9bc55ba0e689d5",
+    "url": "/static/media/Justin_Chao.a1913366.jpg"
+  },
+  {
+    "revision": "eebe1f312797b57eccf0360bb01755dc",
+    "url": "/static/media/Kappas_SF.eebe1f31.jpg"
+  },
+  {
+    "revision": "b09e1b2c9fad05a34a29ef559b7403e2",
+    "url": "/static/media/Kappas_Skates.b09e1b2c.jpg"
+  },
+  {
+    "revision": "e10a09424470d5aff530095527dfaa07",
+    "url": "/static/media/Karen_Ching.e10a0942.jpg"
+  },
+  {
+    "revision": "3b2c3a5f70396255f2cb69dcbd4dc894",
+    "url": "/static/media/Kayla_Wong.3b2c3a5f.jpg"
+  },
+  {
+    "revision": "2060d55ecc2ca8cd607ef055d11ed679",
+    "url": "/static/media/Kaylie_Zhou.2060d55e.jpg"
+  },
+  {
+    "revision": "f26b97ad878bf1ce81bb1f3c780c8505",
+    "url": "/static/media/Kevins_Bday_2020.f26b97ad.JPG"
+  },
+  {
+    "revision": "ca0780aa64ba08ea7b99adfb79f80424",
+    "url": "/static/media/Khoa_Tran.ca0780aa.jpg"
+  },
+  {
+    "revision": "990d31e03512fe1873fbb4d5f1241c03",
+    "url": "/static/media/Koplus.990d31e0.png"
+  },
+  {
+    "revision": "100d3ac9f501307f30698b50e61b71d0",
+    "url": "/static/media/Kpmg.100d3ac9.png"
+  },
+  {
+    "revision": "ee859f791793391630fe25f2d4066e21",
+    "url": "/static/media/LPN_Letters.ee859f79.jpg"
+  },
+  {
+    "revision": "596266275813db17e6bf1304223328c2",
+    "url": "/static/media/LPN_Letters_small.59626627.jpg"
+  },
+  {
+    "revision": "2534f0ad38b9cd461efbcf93575362e1",
+    "url": "/static/media/Lilian_Luu.2534f0ad.jpg"
+  },
+  {
+    "revision": "370a5b375d969f9b9b86f2411fbfab3e",
+    "url": "/static/media/Linda_Woo.370a5b37.jpg"
+  },
+  {
+    "revision": "ae2c640f09d5cc702e3ad00c84e439c3",
+    "url": "/static/media/Liz_Chan.ae2c640f.jpg"
+  },
+  {
+    "revision": "54e024024399d9734935fec9ab6b6d53",
+    "url": "/static/media/Lucian_Liu.54e02402.jpg"
+  },
+  {
+    "revision": "358cbe72b5701e092d8aedc4cd85e630",
+    "url": "/static/media/Lumin_Oo.358cbe72.jpg"
+  },
+  {
+    "revision": "afc9ef5a2335a2236b94d8db016b4c32",
+    "url": "/static/media/MAISS.afc9ef5a.png"
+  },
+  {
+    "revision": "cc34b75d83f0c30b11a960fddc25fa76",
+    "url": "/static/media/MUSA.cc34b75d.jpeg"
+  },
+  {
+    "revision": "d506ca4f2dbd5b4f5bfaf3d98bdc6d73",
+    "url": "/static/media/Mastercard.d506ca4f.png"
+  },
+  {
+    "revision": "6f38dd24339abd3eee76d89e85bc1f9b",
+    "url": "/static/media/MentorGlobalConsultants.6f38dd24.png"
+  },
+  {
+    "revision": "f6a3a5a52901eff9fe2f523a8f5bed20",
+    "url": "/static/media/Microsoft.f6a3a5a5.png"
+  },
+  {
+    "revision": "4e5d8565c5804e31f5b8e939906c0c1e",
+    "url": "/static/media/MongoDB.4e5d8565.png"
+  },
+  {
+    "revision": "4853be8b3be6d21e46869b7bcfe45e22",
+    "url": "/static/media/Moody.4853be8b.png"
+  },
+  {
+    "revision": "1ccbca50a45e7e9f05abc1dfd2d9f049",
+    "url": "/static/media/Mu_Skii.1ccbca50.jpg"
+  },
+  {
+    "revision": "e057bb10359190a43268f5af671c4942",
+    "url": "/static/media/Mu_Thanksgiving.e057bb10.jpg"
+  },
+  {
+    "revision": "813ff0b3dd5e4a6eeb1d200c98921cc6",
+    "url": "/static/media/Naveen_Sastri.813ff0b3.jpg"
+  },
+  {
+    "revision": "7b3ea40bb925b706af2742dfa9f12cf7",
+    "url": "/static/media/Nbc.7b3ea40b.png"
+  },
+  {
+    "revision": "41113a81ba06c1f1bb20a090fa8ccd55",
+    "url": "/static/media/Nicole_Nguyen.41113a81.jpg"
+  },
+  {
+    "revision": "c7f0195616bc0a5ae45c395413c8cc59",
+    "url": "/static/media/Omar_Naji.c7f01956.jpg"
+  },
+  {
+    "revision": "3c805081d2dbacc4e3b40a14f509250d",
+    "url": "/static/media/Omicron_Inside.3c805081.jpg"
+  },
+  {
+    "revision": "ee9c54c5cb569df0cb4eef210c3992f0",
+    "url": "/static/media/Oppti.ee9c54c5.png"
+  },
+  {
+    "revision": "afc8428a74f11bebc982deb9b6e546cf",
+    "url": "/static/media/Pi_Formal.afc8428a.jpg"
+  },
+  {
+    "revision": "16306b67e335e0544ba06305f4343eb3",
+    "url": "/static/media/Picnic.16306b67.JPG"
+  },
+  {
+    "revision": "651da83503240fe8ef2b9da570ef44ab",
+    "url": "/static/media/Pumpkin_Patch.651da835.jpeg"
+  },
+  {
+    "revision": "a5f9eb9077561e7905729e242297a5e0",
+    "url": "/static/media/Raj_Singh.a5f9eb90.jpg"
+  },
+  {
+    "revision": "cd00c454828bcf16131b34fe258938d4",
+    "url": "/static/media/Raymond_Dinh.cd00c454.jpg"
+  },
+  {
+    "revision": "2adec5e06889a6e612c0fe03e36ec7ca",
+    "url": "/static/media/Relationships.2adec5e0.jpg"
+  },
+  {
+    "revision": "30dc1328ffb2076c3274ad40e6e6cdd4",
+    "url": "/static/media/Retreat_Hike.30dc1328.JPG"
+  },
+  {
+    "revision": "919f8dc8e47885587b352cab63d8fa44",
+    "url": "/static/media/RevolutionEntertainment.919f8dc8.webp"
+  },
+  {
+    "revision": "0e4d0903e3cd870db55e5e4e667b6ad7",
+    "url": "/static/media/Rithu_Eswaramoorthy.0e4d0903.jpg"
+  },
+  {
+    "revision": "3a79d1a0747a5d30e029680927f83815",
+    "url": "/static/media/Rubies_Seniors.3a79d1a0.jpg"
+  },
+  {
+    "revision": "5359a7017467862ca85a7db5dbdb1e91",
+    "url": "/static/media/Ruby_Ko.5359a701.jpg"
+  },
+  {
+    "revision": "6abb22cd0c10d478505456e519decb2b",
+    "url": "/static/media/RyanChen_Circle.6abb22cd.jpg"
+  },
+  {
+    "revision": "61ef0506c8b03c5f1e87c90a0b7ee8c4",
+    "url": "/static/media/Ryan_Miranda.61ef0506.jpg"
+  },
+  {
+    "revision": "37b367a499c0ce5488dbb9e78dfd00ff",
+    "url": "/static/media/SMIF.37b367a4.png"
+  },
+  {
+    "revision": "15256412d14e53e73fe67631a2faaf6f",
+    "url": "/static/media/Sahana_Vairavaraj.15256412.jpg"
+  },
+  {
+    "revision": "ef017bea65f458e9bd2af0b377926d1b",
+    "url": "/static/media/Salesforce.ef017bea.png"
+  },
+  {
+    "revision": "f13a2c15aedffc41b9edb0904c6a6e37",
+    "url": "/static/media/Sandy_Nguyen.f13a2c15.jpg"
+  },
+  {
+    "revision": "24e2c5aca6a688f8c6c8553f46e03371",
+    "url": "/static/media/Sapphire_Hiking.24e2c5ac.jpg"
+  },
+  {
+    "revision": "c99f906ed9d4ee668324b198e06daef9",
+    "url": "/static/media/Sapphires_small.c99f906e.jpg"
+  },
+  {
+    "revision": "4293c3af88b042ebd92a7cde79833d4a",
+    "url": "/static/media/SenchaCredit.4293c3af.png"
+  },
+  {
+    "revision": "af78b9d4aa85b10d11441bb537a072de",
+    "url": "/static/media/Sera_Xu.af78b9d4.jpg"
+  },
+  {
+    "revision": "e47b063dc1437cd1eb56854bed56f969",
+    "url": "/static/media/ServiceNow.e47b063d.png"
+  },
+  {
+    "revision": "230503ea15e5b0104b2c64d226072679",
+    "url": "/static/media/Slalom.230503ea.png"
+  },
+  {
+    "revision": "724de4cc7994ded1367ea14ef7badd62",
+    "url": "/static/media/Stephen_Minn.724de4cc.jpg"
+  },
+  {
+    "revision": "c7fbae012be73368aaca38a6959ae747",
+    "url": "/static/media/Sydney_Yu.c7fbae01.jpg"
+  },
+  {
+    "revision": "825bb744d6c61938f25338b92e08dcae",
+    "url": "/static/media/Taiwan_1.825bb744.jpg"
+  },
+  {
+    "revision": "e8f9bb71d8982650c2edf494c17e50f5",
+    "url": "/static/media/Taiwan_2.e8f9bb71.jpg"
+  },
+  {
+    "revision": "2b7b7874daea3267abe798866554fc2d",
+    "url": "/static/media/Talia_Yip.2b7b7874.jpg"
+  },
+  {
+    "revision": "263c0eefb18ef84bd5be95595aebb5cc",
+    "url": "/static/media/Tiffany_Wong.263c0eef.jpg"
+  },
+  {
+    "revision": "79fe9007a3108f4fc0730b1bf9841d2d",
+    "url": "/static/media/TikTok.79fe9007.png"
+  },
+  {
+    "revision": "5285926907830b8a5726e2853654e849",
+    "url": "/static/media/Tiktok.52859269.png"
+  },
+  {
+    "revision": "aaa820930e7639054a979682d897c85d",
+    "url": "/static/media/Topaz_Seniors.aaa82093.jpg"
+  },
+  {
+    "revision": "1347c9a1e6c5b419b374d89ba8903c7a",
+    "url": "/static/media/Twitter.1347c9a1.png"
+  },
+  {
+    "revision": "cd70908f8cda9f0c3c0ae31a17ea1600",
+    "url": "/static/media/UCI_Esports.cd70908f.png"
+  },
+  {
+    "revision": "ddf994b670c9b3909d33c9da8821d1b9",
+    "url": "/static/media/UCI_Medicine.ddf994b6.png"
+  },
+  {
+    "revision": "832323662767c0b436fdf12e427627a2",
+    "url": "/static/media/UFA.83232366.png"
+  },
+  {
+    "revision": "6c3239a81717d9aeb7eb24b509065f4c",
+    "url": "/static/media/Values_Separator.6c3239a8.jpg"
+  },
+  {
+    "revision": "7598b0a92a26bbb8c4674515659fb736",
+    "url": "/static/media/Vivi_Liu.7598b0a9.jpg"
+  },
+  {
+    "revision": "1496fb8ed593a5e7f7ae18b21007dbcb",
+    "url": "/static/media/Vivian_Tran.1496fb8e.jpg"
+  },
+  {
+    "revision": "ad268c78bac22d15961e22ca58d99622",
+    "url": "/static/media/WaltDisney.ad268c78.png"
+  },
+  {
+    "revision": "2ac2d36e6b5353ab44ebc7354f5d8eed",
+    "url": "/static/media/Western_Digital.2ac2d36e.png"
+  },
+  {
+    "revision": "bbf6d626f68e50f45ae527da4f7591a6",
+    "url": "/static/media/Will_Bordessa.bbf6d626.jpg"
+  },
+  {
+    "revision": "de954303fef5d64a76748b98dc1d5905",
+    "url": "/static/media/Winter2022.de954303.png"
+  },
+  {
+    "revision": "9433889c5a24aed2d4d2b215d19e7230",
+    "url": "/static/media/Xi_Casual.9433889c.jpg"
+  },
+  {
+    "revision": "7d62310a515fbd1e1eadd815e6f46583",
+    "url": "/static/media/Xi_Frat_Social.7d62310a.JPG"
+  },
+  {
+    "revision": "940f58ef2f4050e4aa9c7947518a747d",
+    "url": "/static/media/Yllana_Casim.940f58ef.jpg"
+  },
+  {
+    "revision": "f68038e36a790eac4d9dc325b089325e",
+    "url": "/static/media/logo.f68038e3.png"
+  },
+  {
+    "revision": "391d31fd0b9d1a2c61f7049fbd60b0ab",
+    "url": "/static/media/logo_white.391d31fd.png"
+  },
+  {
+    "revision": "0899dbfb88ce956802184573aafbf175",
+    "url": "/static/media/pfp_temp.0899dbfb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ac64d471732dcbfd138dc6dba0dbc35.js"
+  "/precache-manifest.6ae9b9ae3deb10ba07f763b98f4f281e.js"
 );
 
 self.addEventListener('message', (event) => {
