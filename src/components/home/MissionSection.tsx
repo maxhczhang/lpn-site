e1161961cec800ebde9c9ac6fc6791af0299c302
Max Zhang <maxhz1@uci.edu>
2026-03-04 22:48:10 -0800

    updated home page info

--- src/components/home/MissionSection.tsx
+++ src/components/home/MissionSection.tsx
@@ -8,7 +8,7 @@ import { carouselPhotos1 } from "@/lib/data/companyLogos";
 const pillars = [
   {
     title: "Professionalism",
-    body: "With 12 years of history at UCI, we have an expansive network of alumni in different industries and companies. Our Active and Alumni brothers help guide and support you to succeed in the professional world.",
+    body: "With 16+ years of history at UCI, we have an expansive network of alumni in different industries and companies. Our Active and Alumni brothers help guide and support you to succeed in the professional world.",
     link: "/careers",
     linkLabel: "View Careers",
   },
